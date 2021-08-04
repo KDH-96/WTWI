c8a6736f04a0ee87de14125c677f0a5f40cce75c
Esther-jiwon <estheryun0807@gmail.com>
2021-08-04 17:48:37 +0900

    핸드폰 번호 인증 수정

--- wtwiProject/src/main/java/com/wtwi/fin/member/controller/SMSController.java
+++ wtwiProject/src/main/java/com/wtwi/fin/member/controller/SMSController.java
@@ -32,7 +32,7 @@ public class SMSController {
 
 	@RequestMapping(value = "send", method = RequestMethod.POST)
 	public int sendSMS(Member inputMember, Model model) throws Exception { // 휴대폰 문자보내기
-		
+
 		int result = 0;
 		Member member = service.selectPhone(inputMember);
 		
@@ -44,8 +44,8 @@ public class SMSController {
 			
 			// 4 params(to, from, type, text) are mandatory. must be filled
 			HashMap<String, String> params = new HashMap<String, String>();
-			params.put("to", "010-9799-2724");    // 수신전화번호
-			params.put("from", member.getMemberPhone());    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+			params.put("to", inputMember.getMemberPhone());    // 수신전화번호
+			params.put("from", "010-9799-2724");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
 			params.put("type", "SMS");
 			params.put("text", "Where the Weather is... 인증번호는" + "["+checkNum+"]" + "입니다.");
 			params.put("app_version", "test app 1.2"); // application name and version
@@ -53,6 +53,7 @@ public class SMSController {
 			try {
 				JSONObject obj = (JSONObject) coolsms.send(params);
 				result = Integer.parseInt(String.valueOf(obj.get("error_count")));
+
 				if(result == 0) {
 					result = checkNum;
 				} 
